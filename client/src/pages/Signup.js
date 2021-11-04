0d61268ffe43784aac9740ac61362a57f6a4e085
Camil Grabowski <camil.grabowski@uconn.edu>
2021-11-04 16:48:31 -0400

    server problem

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
-  const [addUser, { error }] = useMutation(ADD_USER);     // confused about this
+  const [addUser, { error }] = useMutation(ADD_USER);     
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -16,21 +17,21 @@ const Signup = () => {
     });
   };
 
-  // submit form (notice the async!)
-const handleFormSubmit = async event => {
-  event.preventDefault();
+  // submit form 
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-  // use try/catch instead of promises to handle errors
-  try {
-    // execute addUser mutation and pass in variable data from form
-    const { data } = await addUser({
-      variables: { ...formState }
-    });
-    console.log(data);
-  } catch (e) {
-    console.error(e);
-  }
-};
+    try {
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
 
   return (
     <main className='flex-row justify-center mb-4'>
@@ -78,5 +79,6 @@ const handleFormSubmit = async event => {
     </main>
   );
 };
+
 
 export default Signup;
