2cc54b3c6e3d6570137bfeae3985b53aca1072b1
Camil Grabowski <camil.grabowski@uconn.edu>
2021-11-05 11:15:39 -0400

    browser error

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 import Auth from "../../utils/auth";
 
 const Header = () => {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
@@ -14,7 +19,9 @@ const Header = () => {
           {Auth.loggedIn() ? (
             <>
               <Link to="/profile">Me</Link>
-              <a href="/">Logout</a>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
             </>
           ) : (
             <>
